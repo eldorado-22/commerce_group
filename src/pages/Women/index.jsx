a000c4e69286163a434a17673b4058b20aef89c9
edya_993 <119120263+eldorado-22@users.noreply.github.com>
2023-11-21 11:48:13 +0600

    commerce_women_files_create (#4)

--- src/pages/Women/index.jsx
+++ src/pages/Women/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./style.css"
 
 const Women = () => {
     return (
         <>
           <section>
+            <h1>Women Pages</h1>
             </section>  
         </>
     );
