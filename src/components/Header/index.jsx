38ec17634320f9ed16b3233b0e7df4b297035658
edya_993 <119120263+eldorado-22@users.noreply.github.com>
2023-11-21 11:24:07 +0600

    hero_news_style (#1)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,35 @@ import React from 'react';
 const Header = () => {
     return (
         <>
-          <header>
-            HEADER
+          <header id='header'>
+            <div className="container">
+                <div className="header">
+                    <div className="header-text">
+                        <h1>create x</h1>
+                        <nav className="navbar">
+                            <a href="#">Women</a>
+                            <a href="#">Men</a>
+                            <a href="#">Girls</a>
+                            <a href="#">Boys</a>
+                            <a href="#">Sale</a>
+                        </nav>
+                    </div>
+                    <div className="header-input">
+                        <input type="search" placeholder='Search for products' />
+                        <div className="logos">
+                        <div className="love">
+                            <span>Love</span>
+                            <span>2</span>
+                        </div>
+                        <div className="line"></div>
+                        <div className="basket">
+                            <span>BASKET</span>
+                            <h2>4</h2>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </header>  
         </>
     );
