03b08d11b555dadb97b62990964a662c8e2bef28
eldorado-22 <jumashevv992@gmail.com>
2023-11-21 11:36:05 +0600

    commerce_header_commit

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,23 +10,23 @@ const Header = () => {
                     <div className="header-text">
                         <h1>create x</h1>
                         <nav className="navbar">
-                            <a href="#">Women</a>
+                            {/* <a href="#">Women</a>
                             <a href="#">Men</a>
                             <a href="#">Girls</a>
                             <a href="#">Boys</a>
-                            <a href="#">Sale</a>
+                            <a href="#">Sale</a> */}
                         </nav>
                     </div>
                     <div className="header-input">
                         <input type="search" placeholder='Search for products' />
                         <div className="logos">
                          <div className="love">
-                            <span>Love</span>
+                            {/* <span>Love</span> */}
                             <span>2</span>
                             </div>
                             <div className="line"></div>
                             <div className="basket">
-                            <span>BASKET</span>
+                            {/* <span>BASKET</span> */}
                             <h2>4</h2>
                             </div>
                         </div>
