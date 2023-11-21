735bf27639e33826af423c15c41a59b38b6f5330
eldorado-22 <jumashevv992@gmail.com>
2023-11-21 11:47:44 +0600

    commerce_women_files_create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Header from "./components/Header/index"
 import Footer from "./components/Footer/index"
+import { Route,Routes } from 'react-router-dom';
+import Homes from './pages/Homes';
+import Women from './pages/Women';
+
+
 function App() {
   return (
     <>
      <Header/>
+     <Routes>
+      <Route path="/" element={<Homes/>}/>
+      <Route path="/women" element={<Women/>}/>
+     </Routes>
      <Footer/>
     </>
   );
