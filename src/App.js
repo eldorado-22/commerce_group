9e6e16eea9d28f50ad25b1666b514cba66dd2976
eldorado-22 <jumashevv992@gmail.com>
2023-11-21 11:12:56 +0600

    commerce_group

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./components/Header/index"
+import Footer from "./components/Footer/index"
+function App() {
+  return (
+    <>
+     <Header/>
+     <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Women/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Women = () => {
+    return (
+        <>
+          <section>
+            </section>  
+        </>
+    );
+};
+
+export default Women;
